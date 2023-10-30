9e794c2e45b793dc2280f0a2f2e34934c399a8dc
Kristina <kristina.tibeykina04@mail.ru>
2023-10-30 15:57:10 +0700

    Created model Genre

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+
+    def __str__(self):
+        return self.name
+
+
