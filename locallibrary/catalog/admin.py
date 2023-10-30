d3b900fa5b9676f83e6a30b47d8994ccd9984c33
Kristina <kristina.tibeykina04@mail.ru>
2023-10-30 16:27:13 +0700

    Изменение отображения модели книги, копии книг и авторы на странице администратора

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -4,16 +4,17 @@ from .models import Author, Genre, Book, BookInstance
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
 
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 #admin.site.register(Book)
 #admin.site.register(Author)
