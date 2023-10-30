718c860d6bdb8c649277d7a6478809e709dac520
Kristina <kristina.tibeykina04@mail.ru>
2023-10-30 18:31:18 +0700

    Добавили путь к приложения catalog

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-
+    path('catalog/', include('catalog.urls')),
 ]
