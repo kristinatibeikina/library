5bf0dd4aded8e48065c6cde1e738191c7287a35c
Kristina <kristina.tibeykina04@mail.ru>
2023-10-30 17:00:24 +0700

    .

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
+
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
@@ -29,8 +30,6 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
-#admin.site.register(Book)
-#admin.site.register(Author)
+
 admin.site.register(Genre)
-#admin.site.register(BookInstance)
 admin.site.register(Author, AuthorAdmin)
