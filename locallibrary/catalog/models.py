21bc16bc211c2650612bd8757ee14a64349d71fd
Kristina <kristina.tibeykina04@mail.ru>
2023-10-30 16:01:41 +0700

    Created model Author

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -45,3 +45,16 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return '%s (%s)' % (self.id,self.book.title)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return '%s, %s' % (self.last_name, self.first_name)
