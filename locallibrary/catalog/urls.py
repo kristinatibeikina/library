f63c4edb5f15bc165b46079c01ce1e6a09838ae9
Kristina <kristina.tibeykina04@mail.ru>
2023-10-30 21:16:44 +0700

    Настройка Url пути

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
+from django.urls import re_path as url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
