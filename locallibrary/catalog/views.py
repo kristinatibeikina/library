05e29969bd8fa39c27a3631118e73127f7b2d362
Kristina <kristina.tibeykina04@mail.ru>
2023-10-31 11:41:53 +0700

    Создаем представление (создание, обновление и удаление)

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -12,6 +12,11 @@ import datetime
 
 from .forms import RenewBookForm
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Author
+
 
 class MyView(PermissionRequiredMixin, ):
     permission_required = 'catalog.can_mark_returned'
@@ -73,3 +78,19 @@ def renew_book_librarian(request, pk):
         form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
 
     return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__'
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
