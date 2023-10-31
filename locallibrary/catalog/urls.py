a547a78fc2bec488232d0e76203096dd05aafed6
Kristina <kristina.tibeykina04@mail.ru>
2023-10-31 12:48:14 +0700

    Добавление путей на представление создание, изменение и удаление авторов

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
 from . import views
 from django.urls import re_path as url
+from django.urls import path
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
 ]
