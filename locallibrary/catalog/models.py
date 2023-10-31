08658e6e48f24217fa6513c9348f3ed98e1bffaa
Kristina <kristina.tibeykina04@mail.ru>
2023-10-31 08:54:01 +0700

    Проверка на простроченность сдачи книги

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 import uuid
 from django.contrib.auth.models import User
+from datetime import date
 
 
 class Genre(models.Model):
@@ -52,6 +53,12 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return '%s (%s)' % (self.id,self.book.title)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
 
 class Author(models.Model):
